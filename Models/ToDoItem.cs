efbaedc07909e5a06eeb17d65ca9f6b9211433f8
Suman Humagain <returnzerosh@gmail.com>
2024-09-30 13:16:26 -0400

    Added model todoitem

--- Models/ToDoItem.cs
+++ Models/ToDoItem.cs
@@ -2,6 +2,9 @@
 {
     public class ToDoItem
     {
-
+        public int Id { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string Description { get; set; }
+        public DateTime? CompletedDate { get; set; }
     }
 }
