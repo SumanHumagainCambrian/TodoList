e6944a543e564cb2c1228cd859de8446a5195076
Suman Humagain <returnzerosh@gmail.com>
2024-09-30 13:57:39 -0400

    Committed dbcontext and add service in program file.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add dbContext
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
 
